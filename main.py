6a8c40568d289ea66342601f27589a7b0d26dd35
Ycanzzay <steam3bebra228@gmail.com>
2024-01-04 21:46:03 +0300

    voini

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-N = int(input())
-k = int(input())
-l = list(range(1,N + 1))
-while len(l) !=1:
-    index = k%len(l)
-    k = index; del l[k]; k+=2
-    print(l)
+variors = int(input('Количество воинов '))
+list = list()
+x = 0
+index = 0
+while x < variors:
+    x = x + 1
+    list.append(x)
+k = int(input('Убитый воин '))
+while len(list) > 1:
+    if index != 0:
+        index = len(list) - index
+    else:
+        index = k - 1
+    if index < 0:
+        index = index + k - 1
+    while index <= (len(list) - 1):
+        del list[index]
+        if index == len(list):
+            index = 0
+            break
+        else:
+            index = index + (k - 1)
+print(list)
